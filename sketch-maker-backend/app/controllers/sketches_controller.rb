fff3b92a694b044b8ed66366fa3a02fdbd236657
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-12-14 16:02:43 -0500

    added show to backend

--- sketch-maker-backend/app/controllers/sketches_controller.rb
+++ sketch-maker-backend/app/controllers/sketches_controller.rb
@@ -4,4 +4,9 @@ class SketchesController < ApplicationController
         sketches = Sketch.all
         render :json => sketches
     end
+
+    def show
+        sketch = Sketch.find_by_id(params[:id])
+        render:json => sketch
+    end
 end
