3157d55508c4568f054b2d0518d9d7e30874f859
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-12-14 14:16:17 -0500

    animated p5 text

--- sketch-maker-frontend/src/App.js
+++ sketch-maker-frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       < Sketch />
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
--- sketch-maker-frontend/src/components/sketch.js
+++ sketch-maker-frontend/src/components/sketch.js
@@ -2,37 +2,39 @@ import React from "react";
 import Sketch from "react-p5";
  
 export default (props) => {
+    let angle = 0;
+ 
+    const setup = (p5, canvasParentRef) => {
+        // use parent to render the canvas in this ref
+        // (without that p5 will render the canvas outside of your component)
+        p5.createCanvas(p5.windowWidth, p5.windowHeight*0.995).parent(canvasParentRef);
+        let canvas = document.querySelector("#defaultCanvas0")
+        canvas.toBlob(b => {
+            console.log(blobToFile(b, "img"))
+            // fetch post request with blobToFile(b, "img") as obj
+        })
+        p5.angleMode(p5.DEGREES)
+        p5.rectMode(p5.CENTER)
+        // debugger;
+    };
+ 
+    const draw = (p5) => {
+        p5.background(200, 60, 60);
+        p5.translate(p5.width*0.5, p5.height*0.5);
+        p5.rotate(angle)
+        p5.textAlign(p5.CENTER, p5.CENTER);
+        p5.textSize(30)
+        p5.text("Hello", 0, 0);
+        angle++;
+    };
+
+
 
     const blobToFile = (blob, fileName) => {
         blob.lastModifiedDate = new Date();
         blob.name = fileName;
         return blob
     }
- 
-    const setup = (p5, canvasParentRef) => {
-        // use parent to render the canvas in this ref
-        // (without that p5 will render the canvas outside of your component)
-        p5.createCanvas(500, 500).parent(canvasParentRef);
-        let canvas = document.querySelector("#defaultCanvas0")
-        canvas.toBlob(b => {
-            console.log(blobToFile(b, "img"))
-            // fetch post request with blobToFile(b, "img") as obj
-        })
-        // debugger;
-    };
- 
-    const draw = (p5) => {
-        p5.background(200, 60, 60);
-        p5.textAlign(p5.CENTER);
-        p5.textSize(30)
-        p5.text("Hello", p5.width/2, p5.height/2)
-        // 
-        // p5.ellipse(x, y, 70, 70);
-        // NOTE: Do not use setState in the draw function or in functions that are executed
-        // in the draw function...
-        // please use normal variables or class properties for these purposes
-        // x++;
-    };
  
     return <Sketch setup={setup} draw={draw} />;
 };
