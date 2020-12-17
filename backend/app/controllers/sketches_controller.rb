0e635879db71143e36c0d38576d7d2d1a36d77ea
zkobrinsky <zachkobrinsky@gmail.com>
2020-12-17 11:36:41 -0500

    sketches controller rendering with colors

--- backend/app/controllers/sketches_controller.rb
+++ backend/app/controllers/sketches_controller.rb
@@ -2,19 +2,19 @@ class SketchesController < ApplicationController
 
     def index
         sketches = Sketch.all
-        render json: sketches, methods: :image_url
+        render json: sketches, methods: :image_url, include: {:colors => {only: [:value]}}
     end
 
     def create
-        sketch = Sketch.last
+        sketch = Sketch.last #for debugging only
+        # sketch = Sketch.create(sketch_params) # will be actual create method
         byebug  
         sketch.image.attach(params[:image])
-        render:json => sketch, methods: :image_url
+        render json: sketches, methods: :image_url, include: {:colors => {only: [:value]}}
     end
 
     def show
         sketch = Sketch.find_by_id(params[:id])
-        render:json => sketch, methods: :image_url
-        # render json: {"imageUrl": url_for(sketch.image)}.to_json
+        render json: sketch, methods: :image_url, include: {:colors => {only: [:value]}}
     end
 end
--- backend/app/models/sketch.rb
+++ backend/app/models/sketch.rb
@@ -1,5 +1,10 @@
 class Sketch < ApplicationRecord
+    # alias_attribute :bg_color, :color
+    # has_one :bg_color, through: :colors_sketches
+    # try to set up bg color laters
+    
     has_one_attached :image
+    
     has_many :colors_sketches
     has_many :colors, through: :colors_sketches
     include Rails.application.routes.url_helpers
