91b9c4caafb4b3f555d9e7f8c5ec28951e41758c
zkobrinsky <zachkobrinsky@gmail.com>
2020-12-17 15:24:58 -0500

    stubbed out sketchReducer

---
+++ sketch-maker-frontend/src/redux/reducers/sketchReducer.js
@@ -0,0 +1,11 @@
+const sketchReducer = (state = {sketches: []}, action) => {
+    switch(action.type) {
+        case("FETCH_SKETCHES_SUCCESS"):
+            return {...state, sketches: action.payload};
+        default:
+        return state
+    }
+
+}
+
+export default sketchReducer
