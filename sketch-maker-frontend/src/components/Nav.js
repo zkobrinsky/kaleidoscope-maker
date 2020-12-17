0566f05c81c10d5cc53cb79e0adb3bf024115b20
zkobrinsky <zachkobrinsky@gmail.com>
2020-12-17 15:11:09 -0500

    added redux packages

--- sketch-maker-frontend/src/components/Nav.js
+++ sketch-maker-frontend/src/components/Nav.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import {Link}  from 'react-router-dom'
+// import {Navbar, Form, Button, FormControl} from 'react-bootstrap';   
+
 
 const Nav = () => {
     return (
+        // trying to get bootstrap navbar to work
+//     <Navbar bg="primary" variant="dark">
+//         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+//         <Nav className="mr-auto">
+//         <Nav.Link as={Link} to="/" >Home</Nav.Link>
+//         </Nav>
+//     <Form inline>
+//       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+//       <Button variant="outline-light">Search</Button>
+//     </Form>
+//   </Navbar>
+
+
+
+
+
         <div>
             <ul>
                 <li>
