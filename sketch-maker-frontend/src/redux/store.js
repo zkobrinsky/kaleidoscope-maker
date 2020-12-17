9736d93223545242d61f8995826e92afa0a23ea2
zkobrinsky <zachkobrinsky@gmail.com>
2020-12-17 16:40:40 -0500

    stubbed out store in index.js

--- sketch-maker-frontend/src/redux/store.js
+++ sketch-maker-frontend/src/redux/store.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import sketchReducer from './reducers/sketchReducer';
 import thunk from 'redux-thunk'
 
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() || compose;
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
 
   const rootReducer = combineReducers({
       sketches: sketchReducer
