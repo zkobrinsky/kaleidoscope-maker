14c55b0a5d9ef1ebbabac3e0c582f5a353dd80a0
zkobrinsky <zachkobrinsky@gmail.com>
2020-12-20 17:09:56 -0500

    added options to form

--- sketch-maker-frontend/src/components/CreateSketch.js
+++ sketch-maker-frontend/src/components/CreateSketch.js
@@ -50,6 +50,13 @@ class  CreateSketch extends React.Component {
       <option>3</option>
       <option>4</option>
       <option>5</option>
+      <option>6</option>
+      <option>7</option>
+      <option>8</option>
+      <option>9</option>
+      <option>10</option>
+      <option>11</option>
+      <option>12</option>
     </Form.Control>
   </Form.Group>
 
