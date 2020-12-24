cf491bbc4a77d8c436a467ef323ce3bb81bc75a3
zkobrinsky <zachkobrinsky@gmail.com>
2020-12-24 11:15:59 -0500

    aesthetic changes to sandbox

--- sketch-maker-frontend/src/components/SandboxSketch.js
+++ sketch-maker-frontend/src/components/SandboxSketch.js
@@ -8,16 +8,20 @@ export default (props) => {
   let symmetry = Math.floor(Math.random() * 12) + 1;
   let angle = 360 / symmetry;
   let xoff = 0;
+  let bgAlpha = 0;
   let bg;
+  let blackSwitch = true;
+  
 
   const setup = (p5, canvasParentRef) => {
     // use parent to render the canvas in this ref
     // (without that p5 will render the canvas outside of your component)
-    bg = [p5.random(0,360), p5.random(50,100), p5.random(0,60)]
+    bg = [p5.random(0,360), p5.random(0.5, 1), p5.random(0,0.6)]
+    p5.noCursor()
     p5.createCanvas(p5.windowWidth, p5.windowHeight * 0.915).parent(canvasParentRef);
     p5.angleMode(p5.DEGREES);
-    p5.colorMode(p5.HSL, 360, 100, 100, 100);
-    p5.background(...bg);
+    p5.colorMode(p5.HSL, 360, 1, 1, 1);
+    p5.background(0)
 
     //declare p5 dependent functions here:
     p5.withinCanvas = () => {
@@ -26,10 +30,14 @@ export default (props) => {
   };
   
   const draw = (p5) => {
+    if (bgAlpha < 0.2) {
+    p5.background(0, 0, 0, bgAlpha*-1)
+  }
+    p5.background(...bg, bgAlpha);
+  
     p5.translate(p5.width * 0.5, p5.height * 0.5);
 
     if ( p5.withinCanvas() ) {
-      // console.log("clicked inside canvas")
 
         let mx = p5.mouseX - p5.width / 2;
         let my = p5.mouseY - p5.height / 2;
@@ -37,18 +45,18 @@ export default (props) => {
         let pmy = p5.pmouseY - p5.height / 2;
 
         let hu = p5.map(p5.sin(xoff), -1,1,0,360);
-        let sat = p5.map(p5.sin(xoff), -1,1,60,100);
-        let light = p5.map(p5.sin(xoff), -1,1,30,100);
+        let sat = p5.map(p5.sin(xoff), -1,1,0.6,1);
+        let light = p5.map(p5.sin(xoff), -1,1,0.3,1);
         xoff += 1;
         
-        p5.stroke(hu, sat, light, 100);
+        p5.stroke(hu, sat, light, 1);
 
         for (let i = 0; i < symmetry; i++) {
           let angle = 360 / symmetry;
           p5.rotate(angle);
           let d = p5.dist(mx, my, pmx, pmy)
-          let sw = 8
-          // let sw = p5.map(d, 0, 20, 12, 1) variable strokeweight
+          // let sw = 8
+          let sw = p5.map(d, 0, 20, 12, 1) /* variable strokeweight */
           p5.strokeWeight(sw)
           p5.line(mx, my, pmx, pmy);
           p5.push();
@@ -58,7 +66,17 @@ export default (props) => {
         }
 
     }
+    bgAlpha+= 0.00005;
+    if (p5.frameCount % 30 == 0) {
+      console.log(bgAlpha)
+    }
 
+    if (bgAlpha > 0.02) {
+      bgAlpha += 0.02;
+    }
+    if (bgAlpha > 1) {
+      bgAlpha = 0;
+    }
   };
 
   const windowResized = (p5) => {
@@ -66,10 +84,6 @@ export default (props) => {
     p5.background(...bg);
   };
 
-  const mouseClicked = (p5) => {
-    // postImage()
-  }
 
-
-  return <Sketch setup={setup} draw={draw} windowResized={windowResized} mouseClicked={mouseClicked} />;
+  return <Sketch setup={setup} draw={draw} windowResized={windowResized} />;
 };
